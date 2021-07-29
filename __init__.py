4c1aad9ff24c81d0d2555beafe13d9f3d00780fb
Marc Espín Sanz <mespinsanz@gmail.com>
2021-07-29 10:38:42 +0000

    Updated

--- __init__.py
+++ __init__.py
@@ -6,20 +6,21 @@ from redis import Redis
 file_path =  '../bliss-conf/tango/Pool_demo1.yml'
 file = open(file_path, 'r')
 file_yaml = yaml.safe_load(file)
+file = open(file_path, 'w')
 
 # Redis startup
 db = Redis(unix_socket_path='/tmp/redis.sock')
 
 def run_yaml():
-  file_yaml['device'][1]['properties']['test_val'] = 12345
+  file_yaml['device'][0]['properties']['test_val'] = 12345
   file_str = yaml.dump(file_yaml)
-  file = open(file_path, 'w')
+  file.seek(0)   
   file.write(file_str)
-  return None
+  file.truncate()
 
 def run_redis():
   db.hset('tango.motor.motctrl01.1', 'Velocity', 12345)
   pass
 
-print("YAML = ", timeit(stmt=run_yaml, number = 150))
-print("Redis = ", timeit(stmt=run_redis, number = 150))
+print("YAML = ", timeit(stmt=run_yaml, number = 500))
+print("Redis = ", timeit(stmt=run_redis, number = 500))
